ee9fe9d798021826eb945ccf8e9f71a1125a021c
kambe <nori.k3636@gmail.com>
2021-12-28 00:31:17 +0900

    all playの追加

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import pygame
 import os
 
-def play_music(input):
+def play_music(music_name):
     pygame.mixer.init()
-    if not(os.path.isfile('./midi/'+input+'.mid')):
+    if not(os.path.isfile('./midi/'+music_name+'.mid')):
         return
-    pygame.mixer.music.load('./midi/'+input+'.mid')
+    pygame.mixer.music.load('./midi/'+music_name+'.mid')
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         pygame.time.wait(10)
+    print('music stop')
+
+def all_music():
+    pygame.mixer.init()
+    musics = os.listdir('./music_midi')
+    for midi_file in musics:
+        pygame.mixer.music.load('./midi/'+midi_file)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.wait(10)
     print('music stop')
 
 def stop_music():
     pygame.mixer.music.pause()
     print('music stop')
 
-music = input("曲名を教えて")
-play_music(music)
+
+music = input("なんの曲を流しますか")
+if music == 'all':
+    all_music()
+else:
+    play_music(music)
 
 
 
